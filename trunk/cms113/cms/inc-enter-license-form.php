95cc3a71006a039dcd22a4f4774a163e28248a0e
mailmeyoufool <mailmeyoufool@72a6f21c-4b43-0410-8a18-7fdd8c5540e6>
2008-01-09 16:37:45 +0000

    licence text now prepended to all appropriate files

--- trunk/cms113/cms/inc-enter-license-form.php
+++ trunk/cms113/cms/inc-enter-license-form.php
@@ -1,4 +1,27 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<?php
+/*
+ * ***********************************************************************
+ * Copyright © Ben Hunt 2007, 2008
+ * 
+ * This file is part of cmsfromscratch.
+
+    Cmsfromscratch is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Cmsfromscratch is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Cmsfromscratch.  If not, see <http://www.gnu.org/licenses/>.
+    ***********************************************************************
+ */
+?>
+ 
+ <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 	<title>CMS: Enter licence key</title>
